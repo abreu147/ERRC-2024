3663cf1d572ce49ea99e70dfd51bfcc1f81d8aae
abreu147 <107557349+abreu147@users.noreply.github.com>
2024-07-03 17:30:05 -0300

    componentes

--- todolist/src/components/HeaderF/Header.jsx
+++ todolist/src/components/HeaderF/Header.jsx
@@ -1,12 +1,19 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
+import logo from '../../img/logo-blue-black-small.png'
 export default function Header(){
     return(
         <header>
             <div className="HeaderC">
-                <Link to="/"><h1>Home</h1></Link>
-                <h1>Em criação</h1>
-                <Link to="/Contact"><h1>Contact</h1></Link>
+                <img src={logo} alt="Logo" className="logo-image" />
+                <Link to="/"><h1>Início</h1></Link>
+                <h1>Programação</h1>
+                <Link to="/Contact"><h1>Chamadas</h1></Link>
+                <h1>Inscrição</h1>
+                <h1>Local do Evento</h1>
+                <h1>WRSeg</h1>
+                <h1>Organização</h1>
+                <h1>Premiações</h1>
 
 
             </div>
